784ebcd90482f32eeb488226fc56350f173f8d4f
Flávio Martins <fhcmartins@gmail.com>
2020-04-27 12:40:28 -0300

    Correção Ex_05 Listas

--- mod_10_Ex_05_Listas_02/src/application/Program.java
+++ mod_10_Ex_05_Listas_02/src/application/Program.java
@@ -17,18 +17,21 @@ public class Program {
 		//Lista
 		List<Employee> list = new ArrayList();
 		
+		//Inserindo dados
+		
 		//Lento um numero inteiro N
 		System.out.print("How many employees will be registered? ");
 		int n = sc.nextInt();
 		sc.nextLine();
 		
 		//Inserindo informacoes de acordo com N
-		for(int i = 0; i < n; i++) {
+		System.out.println();
+		for(int i = 1; i <= n; i++) {
 			System.out.println("Employee #" + i + ":");
 			System.out.print("Id: ");
 			int id = sc.nextInt();
 			//Verificando repeticao de Id
-			if(id == Employee.id) {
+			while(hasId(list, id)) {
 				System.out.println("Id already taken. Try again: ");
 				id = sc.nextInt();
 			}
@@ -41,8 +44,33 @@ public class Program {
 			list.add(new Employee(id, name, salary));			
 		}
 		
+		//Calculando os salarios
+		System.out.println();
+		System.out.print("Enter the employee id that will have salary increase: ");
+		int id = sc.nextInt();
+		
+		//filtrando
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		
+		if(emp == null) {
+			System.out.print("Enter the percentage: ");
+			double percentage = sc.nextDouble();
+			emp.increaseSalary(percentage);
+		}
+		
+		//Listando Empregados
+		System.out.println();
+		System.out.println("List of Employees:");
+		for(Employee obj : list) {
+			System.out.println(obj);
+		}
 		
 		sc.close();
+	}
+	
+	public static boolean hasId(List<Employee> list, int id) {
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return emp != null;
 	}
 
 }
--- mod_10_Ex_05_Listas_02/src/entities/Employee.java
+++ mod_10_Ex_05_Listas_02/src/entities/Employee.java
@@ -11,7 +11,7 @@ public class Employee {
 	}
 	
 	//Constructor
-	public Employee() {
+	public Employee(Integer id, String name, Double salary) {
 		this.id = id;
 		this.name = name;
 		this.salary = salary;
@@ -45,7 +45,7 @@ public class Employee {
 	
 	//Calculo do salario
 	public void increaseSalary(double percentage) {
-		salary += salary * percentage;
+		salary += (salary * percentage) / 100;
 	}
 	
 	//toString
