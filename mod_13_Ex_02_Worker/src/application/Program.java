69363f78e5b78dffb0e3193401c61da4053b88f7
Flávio Martins <fhcmartins@gmail.com>
2020-08-13 12:49:10 -0300

    comitando program_worker

--- mod_13_Ex_02_Worker/src/application/Program.java
+++ mod_13_Ex_02_Worker/src/application/Program.java
@@ -1,10 +1,33 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Worker;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter department's name: ");
+		String departmentName = sc.nextLine();
+		System.out.print("Enter worker data: ");
+		System.out.print("Name:");
+		String workerName = sc.nextLine();
+		System.out.print("Level: ");
+		String workerLevel = sc.nextLine();
+		System.out.print("Base Salary");
+		double baseSalary = sc.nextDouble();
+		
+		Worker worker = new Worker(workerName, WorkerLevel.);
+		
+		
+		
+		sc.close();
 	}
 
 }
